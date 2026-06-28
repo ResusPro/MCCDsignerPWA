0322f2119b69dd2cf5f03708430227d965f4a2f5
dr_nick <dr.nick.foster80@gmail.com>
2026-06-28 12:47:18 +0100

    0.3a

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'mccdsigner-pwa-v0.3-static-v1';
+const CACHE_NAME = 'mccdsigner-pwa-v0.3a-static-v1';
 const CORE = [
   './',
   './index.html',
   './manifest.webmanifest',
   './privacy.txt',
-  './assets/app-v0.3.js',
-  './assets/app-v0.3.css',
+  './assets/app-v0.3a.js',
+  './assets/app-v0.3a.css',
   './assets/pdf.worker.mjs',
   './icons/icon-192.png',
   './icons/icon-512.png',
